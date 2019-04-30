eb3bf0e8badb236a97e7371f15b84b21d9387e4f
github username <github email address>
2019-04-30 10:48:07 -0700

    invalid api key

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,27 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = ({reviews: [], searchTerm: []})
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(data =>
+
+      this.setState({reviews: data}))
+  }
+
+  render(){
+    return (
+      <div className='searchable-movie-reviews'>
+        <h2>Reviews for {this.state.searchTerm} </h2>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
